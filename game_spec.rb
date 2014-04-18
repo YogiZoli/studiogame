4f480f65090c49c2418f3a23fa7b95175c03abfc
YogiZoli <zoltanp.gero@gmail.com>
2014-04-18 21:53:23 +0100

    Game spec tests different behavior for different rolled numbers with Die

--- game_spec.rb
+++ game_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'game'
+require_relative 'die'
 
 describe Game do 
 
@@ -6,9 +7,32 @@ describe Game do
 		@game = Game.new("Knuckleheads")
 
 		@initial_health = 100
-		@player = Player.new("moe", initial_health)
+		@player = Player.new("moe", @initial_health)
 
 		@game.add_player(@player)
 	end
 	
+	it "should increase health with 15 if high number is rolled" do
+		Die.any_instance.stub(:roll).and_return(5)
+
+		@game.play
+
+		@player.health.should == @initial_health + 15
+	end
+
+	it "skips the player if a medium number is rolled" do
+		Die.any_instance.stub(:roll).and_return(3)
+
+		@game.play
+
+		@player.health.should == @initial_health
+	end
+
+	it "should decrease health with 10 if a low number is rolled" do
+		Die.any_instance.stub(:roll).and_return(1)
+
+		@game.play
+
+		@player.health.should == @initial_health - 10
+	end
 end
